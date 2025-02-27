6addc538c59c720c1ab0db775ae2d466692b43ec
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-02-27 15:23:05 +0100

    Amelioration du formulaire en ajoutant le bouton se connecter avec github et mise a jour des couleurs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react";
+import Login from "./pages/Login";
+
 function App() {
-	return (
-		<div className='flex justify-center items-center min-h-screen bg-blue-900'>
-			<h1 className='text-3xl font-bold underline text-white'>
-				Ce projet est un template React avec Tailwind CSS
-			</h1>
-		</div>
-	)
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
-export default App
+export default App;
