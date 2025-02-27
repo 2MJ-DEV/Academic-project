9dfbaefcc0d3eb3e6ff38d31d7cc9e81038a65c5
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-02-27 12:58:35 +0100

    Initial commit #1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='flex justify-center items-center min-h-screen bg-blue-900'>
+			<h1 className='text-3xl font-bold underline text-white'>
+				Ce projet est un template React avec Tailwind CSS
+			</h1>
+		</div>
+	)
+}
+
+export default App
